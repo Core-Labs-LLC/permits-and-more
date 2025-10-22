be35399d9c3bc1faca024f130b49cce667102236
jgierlach <jegierlach@gmail.com>
2025-10-22 17:02:11 -0500

    Add Tailwind CSS configuration file and integrate forms and typography plugins

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	plugins: [forms, typography]
+};
