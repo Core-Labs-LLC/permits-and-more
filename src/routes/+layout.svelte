269fcc96d8def75d7e7e007c5f5bc78d8a420526
jgierlach <jegierlach@gmail.com>
2025-10-22 16:55:53 -0500

    First commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '../app.css';
+
+	/** @type {{children: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Navbar />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
